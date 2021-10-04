67ed3e17cfe7a74b5566a0185635c6c4c80aca6b
Jamie iOS <>
2021-10-05 00:59:27 +0700

    feat: ninth page

---
+++ lib/pages/ninth_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+class NinethPage extends StatefulWidget {
+  const NinethPage({Key key}) : super(key: key);
+
+  @override
+  _NinethPageState createState() => _NinethPageState();
+}
+
+class _NinethPageState extends State<NinethPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+        ),
+        child: _scale(),
+      ),
+    );
+  }
+
+  Widget _scale() {
+    return SizedBox(
+      child: DefaultTextStyle(
+        style: const TextStyle(
+          fontSize: 50.0,
+          fontFamily: 'SF',
+        ),
+        child: Center(
+          child: ScaleAnimatedTextKit(
+            repeatForever: true,
+            text: [
+              'Stay Hungry',
+              'Stay Foolish',
+            ],
+            textStyle: TextStyle(
+              color: Colors.black,
+            ),
+            duration: Duration(seconds: 2),
+          ),
+        ),
+      ),
+    );
+  }
+}
