83bbf8a1107b5c6a76886a9ff40607f6d7be36e6
Jamie iOS <>
2021-10-17 15:42:22 +0700

    eleven page

---
+++ lib/pages/eleven_page.dart
@@ -0,0 +1,30 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+
+class ElevenPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Eleven'),
+      ),
+      body: Container(
+        child: Center(
+          child: SizedBox(
+            width: 350.0,
+            child: TextLiquidFill(
+              text: 'LIQUIDY',
+              waveColor: Colors.blueAccent,
+              boxBackgroundColor: Colors.redAccent,
+              textStyle: TextStyle(
+                fontSize: 80.0,
+                fontWeight: FontWeight.bold,
+              ),
+              boxHeight: 300.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
